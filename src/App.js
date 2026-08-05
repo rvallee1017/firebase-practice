23245e8bac92771eeb68ce1451d20b8160fd4410
unknown <rachaelvallee2019@gmail.com>
2026-08-05 12:51:11 -0500

    added code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { auth } from './firebase/init.js';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
 
 function App() {
+  const [user, setUser] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      if (user) {
+        setUser(user);
+      }
+    });
+  }, []);
+
+  function register() {
+    createUserWithEmailAndPassword(auth, 'user@example.com', 'password')
+      .then((user) => {
+        console.log('User registered:', user);
+      })
+      .catch((error) => {
+        console.error('Error registering user:', error);
+      });
+  }
+
+  function login() {
+    signInWithEmailAndPassword(auth, 'user@example.com', 'password')
+      .then(({user}) => {
+        setUser(user);
+      })
+      .catch((error) => {
+        console.error('This password is incorrect or the user does not exist:', error);
+      });
+  }
+
+  function logout() {
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.error('Error logging out:', error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {loading ? 'Loading...' : user ? <p>Welcome, {user.email}</p> : <p>Please log in.</p>}
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/firebase/init.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNzyDulQ-OI925fpouUF0-pfnf1tzW5PI",
+  authDomain: "fir-practice-a7ae2.firebaseapp.com",
+  projectId: "fir-practice-a7ae2",
+  storageBucket: "fir-practice-a7ae2.firebasestorage.app",
+  messagingSenderId: "938134766754",
+  appId: "1:938134766754:web:ced3ac5a413dd36c7f03fb"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
